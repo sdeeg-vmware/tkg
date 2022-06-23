cdd6cdc8345485817d0b0bfa08783a035a0a99d3
sdeeg-vmware <sdeeg@vmware.com>
2022-06-23 22:55:19 +0000

    added kapp and contour to tex

--- Readme.md
+++ Readme.md
@@ -7,14 +7,32 @@ My set of tools I use to deploy and manage TKG (mostly TKGs) from a Linux "jumpb
 Assumes TANZU_NET_USER and TANZU_NET_PASSWORD are set in the user env or sourceable from a script 
 ($HOME/Documents/TanzuNet_creds).
 
-TKG essentials and extensions (downloaded, expanded to downloads, apps copied to PATH eg: /usr/local/bin).  Extensions for
-TKGs are set to 1.3.1 as per doc.
+TKG essentials and extensions (downloaded, expanded to downloads directory, apps copied to PATH eg: /usr/local/bin).  
+Extensions for TKGs are 1.3.1 as per doc.
 
 Add the bin directory to your path in .profile
 
 if [ -d "$HOME/tkg/bin" ] ; then
     PATH="$HOME/tkg/bin:$PATH"
 fi
+
+## Tanzu Extensions (tex)
+
+From a raw cluster install: cert-manager, kapp, contour, service-discovery/external-dns
+
+LCM with install|update|delete
+
+Keep any config files modified from templates in tkg/config
+
+### Contour
+I copied the default configuration using a LoadBalancer to our config files, but didn't modify it.
+
+cp downloads/tkg-extensions-v1.3.1+vmware.1/extensions/ingress/contour/vsphere/contour-data-values-lb.yaml.example config/contour/vsphere/contour-data-values-lb.yaml
+
+### Service Discovery / External DNS
+Copy the default configuration and modify.
+
+cp downloads/tkg-extensions-v1.3.1+vmware.1/extensions/service-discovery/external-dns/external-dns-data-values-rfc2136-with-contour.yaml.example ./config/external-dns/
 
 ## Create Cluster
 
--- bin/tex.sh
+++ bin/tex.sh
@@ -9,21 +9,23 @@ EX_CERT_LOCATION=$EX_LOCATION/cert-manager
 
 EX_KAPP_LOCATION=$EX_LOCATION/extensions
 
-EX_DNS_LOCATION=$EX_LOCATION/extensions/service-discovery/external-dns
-EX_DNS_DATA_FILE=$EX_CONFIG/external-dns/external-dns-data-values-rfc2136-contour.yaml
+EX_CONTOUR_LOCATION=$EX_LOCATION/extensions/ingress/contour
+EX_CONTOUR_DATA_FILE=$EX_CONFIG/contour/vsphere/contour-data-values-lb.yaml
 
+EX_DNS_LOCATION=$EX_LOCATION/extensions/service-discovery/external-dns
+EX_DNS_DATA_FILE=$EX_CONFIG/external-dns/external-dns-data-values-rfc2136-with-contour.yaml
 
 case $1 in
     cert-manager )
         case $2 in
             install )
-                kubectl apply -f ${EX_CERT_LOCATION}
+                kubectl apply -f ${EX_CERT_LOCATION}/
                 ;;
             update )
-                kubectl apply -f ${EX_CERT_LOCATION}
+                kubectl apply -f ${EX_CERT_LOCATION}/
                 ;;
             uninstall )
-                kubectl delete -f ${EX_CERT_LOCATION}
+                kubectl delete -f ${EX_CERT_LOCATION}/
                 ;;
 
             * )
@@ -48,15 +50,35 @@ case $1 in
                 ;;
         esac
         ;;
+    contour )
+        case $2 in
+            install )
+                kubectl apply -f ${EX_CONTOUR_LOCATION}/namespace-role.yaml
+                kubectl create secret generic contour-data-values --from-file=values.yaml=${EX_CONTOUR_DATA_FILE} -n tanzu-system-ingress
+                kubectl apply -f ${EX_CONTOUR_LOCATION}/contour-extension.yaml
+                ;;
+            update )
+                kubectl create secret generic contour-data-values --from-file=values.yaml=${EX_CONTOUR_DATA_FILE} -n tanzu-system-ingress -o yaml --dry-run | kubectl replace -f-
+                ;;
+            uninstall )
+                kubectl delete -f ${EX_CONTOUR_LOCATION}/contour-extension.yaml
+                kubectl delete -f ${EX_CONTOUR_LOCATION}/namespace-role.yaml
+                ;;
+
+            * )
+                echo "fallout"
+                ;;
+        esac
+        ;;
     dns )
         case $2 in
             install )
                 kubectl apply -f ${EX_DNS_LOCATION}/namespace-role.yaml
-                kubectl create secret generic external-dns-data-values --from-file=values.yaml=${EX_DNS_LOCATION}/${EX_DNS_DATA_FILE} -n tanzu-system-service-discovery
+                kubectl create secret generic external-dns-data-values --from-file=values.yaml=${EX_DNS_DATA_FILE} -n tanzu-system-service-discovery
                 kubectl apply -f ${EX_DNS_LOCATION}/external-dns-extension.yaml
                 ;;
             update )
-                kubectl create secret generic external-dns-data-values --from-file=values.yaml=${EX_DNS_LOCATION}/${EX_DNS_DATA_FILE} -n tanzu-system-service-discovery -o yaml --dry-run | kubectl replace -f-
+                kubectl create secret generic external-dns-data-values --from-file=values.yaml=${EX_DNS_DATA_FILE} -n tanzu-system-service-discovery -o yaml --dry-run | kubectl replace -f-
                 ;;
             uninstall )
                 kubectl delete -f ${EX_DNS_LOCATION}/external-dns-extension.yaml
@@ -69,6 +91,6 @@ case $1 in
         esac
         ;;
     * )
-        echo "usage: tex.sh [cert-manager|kapp|dns] [install|update|delete]"
+        echo "usage: tex.sh [cert-manager|kapp|contour|dns] [install|update|delete]"
         ;;
 esac
